f59217347b1e120eede5a40b4c4fd9b5000241cf
Walter Donda <walteroscardonda@gmail.com>
2022-04-16 21:55:53 -0300

    Add files via upload
    Creación del repositorio para reflejar mejoras del módulo

---
+++ custom_crm/models/visit.py
@@ -0,0 +1,22 @@
+from odoo import fields, models
+
+
+class Visit(models.Model):
+    _name = 'custom_crm.visit'
+    _rec_name = 'name'
+    _description = 'Comercial visit'
+
+    name = fields.Char(string='Descripción')
+    customer = fields.Many2one(string='Cliente', comodel_name='res.partner')
+    date = fields.Datetime(string='Fecha de visita')
+    type = fields.Selection(
+        [('P', 'Presencial'), ('R', 'Remota')],
+        string='Tipo de visita', required=True)
+    code = fields.Char(string='Código de visita',
+                       help='Código único de soporte o implementación',
+                       index=True,
+                       required=True)
+    done = fields.Boolean(string='Realizada?')
+
+    def toggle_state_visit(self):
+        self.done = not self.done
